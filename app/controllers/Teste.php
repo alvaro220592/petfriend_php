2cd153ef90349617272fb4f30c5946af1400cd78
alvaro220592 <alvaro220592@gmail.com>
2022-03-20 00:55:17 -0300

    Implantação da ferramenta phpdotenv

--- app/controllers/Teste.php
+++ app/controllers/Teste.php
@@ -2,8 +2,17 @@
 
 namespace App\controllers;
 
+use Dotenv\Dotenv;
+
 class Teste{
     public function index(){
         return "Autoload OK";
+    }
+
+    public function env($dir)
+    {
+        $env = Dotenv::createUnsafeImmutable($dir);
+        $env->load();
+        return getenv('TESTE');
     }
 }
